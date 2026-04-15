b4da832a8f64e638456585bbcc47eba9d27f8129
brokali451 <brokali451@gmail.com>
2026-04-15 22:36:53 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using Storage_HW4;
+using System;
+using System.Collections.Generic;
+
+namespace SmartWarehouse.App;
+
+public class Program
+{
+    public static void Main()
+    {
+        Warehouse warehouse = new Warehouse(10);
+
+        warehouse.OnItemChanged += (cellIndex, itemName) =>
+            Console.WriteLine($"Log: in storage {cellIndex} added an item: {itemName}");
+
+        List<string> products = new()
+        {
+            "keyboard",
+            "mouse",
+            "",
+            "keyboard",
+            "monitor"
+        };
+
+        Predicate<string> isValidProduct = productName => !string.IsNullOrWhiteSpace(productName);
+        Func<string, string> formatProductName = productName => productName.ToUpper();
+
+        int currentCell = 0;
+
+        foreach (string product in products)
+        {
+            if (!isValidProduct(product))
+            {
+                Console.WriteLine("Skipped empty name of the product.");
+                continue;
+            }
+
+            if (currentCell >= warehouse.Capacity)
+            {
+                Console.WriteLine("There are no free storages.");
+                break;
+            }
+
+            warehouse[currentCell] = formatProductName(product);
+            currentCell++;
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("Storage info:");
+
+        for (int i = 0; i < warehouse.Capacity; i++)
+        {
+            string item = warehouse[i];
+            Console.WriteLine($"Storage #{i}: {(string.IsNullOrEmpty(item) ? "empty" : item)}");
+        }
+    }
+}
---
+++ Warehouse.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System;
+
+namespace Storage_HW4;
+
+public class Warehouse
+{
+    private readonly string[] _items;
+
+    public event Action<int, string>? OnItemChanged;
+
+    public Warehouse(int capacity = 10)
+    {
+        if (capacity <= 0)
+        {
+            throw new ArgumentException("Storage has to be more then 0.");
+        }
+
+        _items = new string[capacity];
+    }
+
+    public string this[int index]
+    {
+        get
+        {
+            ValidateIndex(index);
+            return _items[index];
+        }
+        set
+        {
+            ValidateIndex(index);
+            _items[index] = value;
+            OnItemChanged?.Invoke(index, value);
+        }
+    }
+
+    public int Capacity => _items.Length;
+
+    private void ValidateIndex(int index)
+    {
+        if (index < 0 || index >= _items.Length)
+        {
+            throw new IndexOutOfRangeException("This index is already existing.");
+        }
+    }
+}
